7168f293b481ff75c3a3f1bc93a091d11ceb78fa
pragatichaturvedi1992 <77502679+pragatichaturvedi1992@users.noreply.github.com>
2021-07-10 16:13:43 -0300

    pushing Pending Files

---
+++ HotelReservation/src/main/java/com/example/HotelReservation/entities/HotelDetails.java
@@ -0,0 +1,32 @@
+package com.example.HotelReservation.entities;
+
+public class HotelDetails {
+
+    private String hotel_name;
+    private int price;
+    private boolean availability;
+
+    public String getHotel_name() {
+        return hotel_name;
+    }
+
+    public void setHotel_name(String hotel_name) {
+        this.hotel_name = hotel_name;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    public boolean isAvailability() {
+        return availability;
+    }
+
+    public void setAvailability(boolean availability) {
+        this.availability = availability;
+    }
+}
